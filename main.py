5bc1969ced68de07da5cc924c40d433b935c8e01
aiupt <ulutas.alperen@outlook.com>
2025-02-01 21:54:20 +0300

    add player

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import Player
+
+
 
 def game_loop(screen):
     clock = pygame.time.Clock() # add clock
     dt = 0
+    player1 = Player(SCREEN_WIDTH /2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0))
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(FRAME_RATE) / 1000  # FRAME_RATE = 60
 
+
+        
 
 
 

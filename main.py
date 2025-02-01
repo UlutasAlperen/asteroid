0dc83bbe24da658c5a9976bfb04088c6938c2aba
aiupt <ulutas.alperen@outlook.com>
2025-02-02 01:49:25 +0300

    create containers

--- main.py
+++ main.py
@@ -7,15 +7,26 @@ from player import Player
 def game_loop(screen):
     clock = pygame.time.Clock() # add clock
     dt = 0
-    player1 = Player(SCREEN_WIDTH /2,SCREEN_HEIGHT/2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable) 
+
+    player = Player(SCREEN_WIDTH /2,SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0))
-        player1.update(dt)
-        player1.draw(screen)
+
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(FRAME_RATE) / 1000  # FRAME_RATE = 60
 

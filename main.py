74df4be6a4349db233cf68b42fdd14b72de563ca
aiupt <ulutas.alperen@outlook.com>
2025-02-01 18:22:59 +0300

    display screen

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import pygame
 from constants import *
 
+def game_loop(screen):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    game_loop(screen)
+
 
 
 if __name__=="__main__":
     main()
+
+
+
+
+
